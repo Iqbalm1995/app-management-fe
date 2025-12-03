54a54b7ef10741dee15af5a51f6bf62a25f0f931
BANKBJB\n333 <refanzalt@gmail.com>
2025-12-03 08:50:47 +0700

    cp

--- src/app/(pages)/requirements/register-form/formRegsiterRequirements.tsx
+++ src/app/(pages)/requirements/register-form/formRegsiterRequirements.tsx
@@ -6596,10 +6596,10 @@ const Section4RFCView = ({
     const updatedBacklogData: ReqBacklogPayload[] = BacklogChanges.map(
       (dt) => ({
         // backlogId: generateFakeId(),
-        parentBacklogId: dt.backlog.id,
-        backlogName: dt.changes?.backlogName || "",
-        backlogDesc: dt.changes?.backlogDesc || "",
-        note: dt.changes?.note || "",
+        parentBacklogId: dt.backlog.id === "NEW_SCOPE" ? null : dt.backlog.id,
+        backlogName: dt.backlog.id === "NEW_SCOPE" ? (dt.backlog.backlogName || dt.changes?.backlogName || "") : (dt.changes?.backlogName || ""),
+        backlogDesc: dt.backlog.id === "NEW_SCOPE" ? (dt.backlog.backlogDesc || dt.changes?.backlogDesc || "") : (dt.changes?.backlogDesc || ""),
+        note: dt.backlog.id === "NEW_SCOPE" ? (dt.backlog.note || dt.changes?.note || "") : (dt.changes?.note || ""),
         posOrder: dt.changes?.posOrder || DataBackLogs.length + 1,
       })
     );
